1abe950ca32e0acc7b46bb8b4f96c1a01792e6be
DeminRDru <140727457+DeminRDru@users.noreply.github.com>
2025-10-06 20:06:22 +0300

    Update kde_neon.sh
    ,,,,

--- kde_neon.sh
+++ kde_neon.sh
@@ -6,3 +6,16 @@ echo ""
 echo "---> Прежде чем начать, советую глянуть сюда: https://serviceinf.ru/"
 echo ""
 echo "| Итак, приступимс:"
+
+
+#Обновление системы после установки:
+
+echo ""
+read -p ">>> Обновимся? (y/n) " choice
+echo ""
+if [ "$choice" == "y" ]; then
+    sudo apt upgrade 
+    sudo apt full-upgrade -y
+    sudo apt autoremove -y
+    echo ""
+    echo "| Отлично! Система обновилась, даже немного почистилась, теперь можно продолжить!"
